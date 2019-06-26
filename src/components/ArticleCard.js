37e5d5128707f360d8b7412fdd74d63e1fb9f914
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-06-26 10:21:57 +0100

    added User component to ArticleCard component

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,13 +2,16 @@ import * as React from "react";
 import moment from "moment";
 import styles from "../style/ArticleCard.module.css";
 import { Link } from "@reach/router";
+import User from "./User";
 
 export const ArticleCard = ({ article_id, article }) => {
   const rawDate = article.created_at;
   const formattedDate = moment(rawDate).format("YYYY MM DD");
+  const username = article.author;
 
   return (
     <div className={styles.articleCard}>
+      <User username={username} />
       <Link to={`/articles/${article_id}`}>
         <h2 className={styles.articleTitle}>{article.title}</h2>
         <h3>Topic: {article.topic}</h3>
