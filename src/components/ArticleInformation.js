6eedadcb43328efb5b45caf48acd62e89c9aeb90
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-06-29 16:46:25 +0100

    changed article info to stateless component

--- src/components/ArticleInformation.js
+++ src/components/ArticleInformation.js
@@ -2,23 +2,25 @@ import React, { Component } from "react";
 import Vote from "./Vote.js";
 import styles from "../style/ArticleInformation.module.css";
 
-class ArticleInformation extends Component {
-  render() {
-    const { article, article_id, handleVote, votes, voteChange } = this.props;
-
-    return (
-      <section className={styles.body}>
-        <Vote
-          article_id={article_id}
-          votes={votes}
-          handleVote={handleVote}
-          votes={votes}
-          voteChange={voteChange}
-        />
-        <p>Comments: {article.comment_count}</p>
-      </section>
-    );
-  }
-}
+export const ArticleInformation = ({
+  article,
+  article_id,
+  handleVote,
+  votes,
+  voteChange
+}) => {
+  return (
+    <section className={styles.body}>
+      <Vote
+        article_id={article_id}
+        votes={votes}
+        handleVote={handleVote}
+        votes={votes}
+        voteChange={voteChange}
+      />
+      <p>Comments: {article.comment_count}</p>
+    </section>
+  );
+};
 
 export default ArticleInformation;
